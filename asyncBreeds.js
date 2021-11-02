2a9321e6fdbaa6cd9af7fcc7f6488a9918597d9e
Drumshtick <ekodnar@outlook.com>
2021-11-02 22:59:31 +0000

    Created tests for sync and async functions with mocha and chai

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -3,10 +3,14 @@ const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
-  let data = fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) callback(data, breed);
+    if (!error) {
+      callback(data, breed);
+    } else {
+      callback(undefined, breed);
+    }
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
@@ -17,6 +21,4 @@ const printData = (data, breed) => {
   console.log(`${breed}: ${data}`);
 };
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay', printData);
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+module.exports = breedDetailsFromFile;
